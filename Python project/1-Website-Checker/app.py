445897790da1b94ce6c14903d519e0c10995764e
Maddi Chaitanya <maddichaitanya14@gmail.com>
2025-06-08 16:43:43 +0530

    My first python project WEBSITE URL CHECKER

---
+++ Python project/1-Website-Checker/app.py
@@ -0,0 +1,10 @@
+print("🔎  WEBSITE URL CHECKER  🔎")
+
+url = input("\nEnter a Website URL : ")
+
+if url.startswith("https://"):
+    print("\n🔐 This Website uses URL HTTPS ( Secure 🔐 )")
+elif url.startswith("http://"):
+    print("\n🔓 This Website uses URL HTTP ( not Secure 🔓 )")
+else:
+    print("\n❌ Enter URL is not valide : ❌")
