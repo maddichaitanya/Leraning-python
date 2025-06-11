f51607b58b998c8ec1f94516bd399c7b5c8561f7
Maddi Chaitanya <maddichaitanya14@gmail.com>
2025-06-11 13:58:37 +0530

    Python project 9 Reverse the name

---
+++ Python project/9-Reverse-name/app.py
@@ -0,0 +1,13 @@
+print(" ↔️  REVERSEING THE NAME ↔️")
+
+while True:
+    name = input(" Enter Name : ")
+    if not name:
+        break
+    reverse_name = name[::-1]
+    print(f"{name} Reverseing the name : {reverse_name}")
+    sel = input("\nDo you want to enter another name (y/n): ")
+    if sel != "y":
+        break
+
+# output :- https://app.eraser.io/workspace/lUk2zr7D6SdyPceB6K7Q?origin=share
