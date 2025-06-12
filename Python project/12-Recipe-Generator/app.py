41de38142cc29f15be9ca54900afcc5c6faae008
Maddi Chaitanya <maddichaitanya14@gmail.com>
2025-06-12 15:25:30 +0530

    python 12 th project Recipe Generator

---
+++ Python project/12-Recipe-Generator/app.py
@@ -0,0 +1,24 @@
+import random
+
+print(" 🧑 🍜 Random Recipe Generator 🍩 ")
+
+protines = ["Chiken", "beef", "tofu", "frish", "eggs"]
+veggies = ["carrot", "mushroom", "bell peppers", "broccoli"]
+carbs = ["rice", "pasta", "braed", "potatoes", "quinoa"]
+methods = ["roasted", "baked", "grilled", "stir-fried", "sauteed", "plain"]
+flovors = ["Sweet", "garlic", "lemon", "spicy", "herb"]
+while True:
+    protine = random.choice(protines)
+    veggie = random.choice(veggies)
+    carb = random.choice(carbs)
+    method = random.choice(methods)
+    flovor = random.choice(flovors)
+
+    print(
+        f"\n Your random recipe : {flovor}{method} {protine} with {veggie} and {carb}")
+
+    choice = input("\nDo you Want to continue y/n : ")
+    if choice.lower() != "y":
+        break
+
+# output:- https://app.eraser.io/workspace/lUk2zr7D6SdyPceB6K7Q?origin=share
